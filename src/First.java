40006730aec501ae3c1e0df2dccde55198040726
nizi <failaforcontact@gmail.com>
2020-08-13 21:58:49 +0900

    first commit

---
+++ src/First.java
@@ -0,0 +1,14 @@
+public class First {
+  public static void main(String[] args) {
+    System.out.println(60 * 24 * 24);
+    int age;
+    age = 30;
+    System.out.println(age);
+    final double pi = 3.14;
+    System.out.println(pi);
+    String escape = "私の好きな記号は\"";
+    System.out.println(escape);
+    int cast = (int)3.2;
+    System.out.println(cast);
+  }
+}
