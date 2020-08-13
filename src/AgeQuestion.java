6ddcf491f41a4d2e3888ebc79be5ac4116c0d26c
nizi <failaforcontact@gmail.com>
2020-08-14 08:57:11 +0900

    FortuneTellingを追加

---
+++ src/AgeQuestion.java
@@ -0,0 +1,7 @@
+public class AgeQuestion {
+  public static void main(String[] args) {
+    System.out.println("あなたの名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("ようこそ、" + name + "さん");
+  }
+}
